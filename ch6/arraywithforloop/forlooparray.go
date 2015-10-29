656cc10cb323931d551dd65e8ca08a02c5a7d7a6
bossco <bossco28@gmail.com>
2015-10-29 13:37:48 -0400

    for loops and ranges with arrays

--- ch6/arraywithforloop/forlooparray.go
+++ ch6/arraywithforloop/forlooparray.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var x [5]float64
+	var x [6]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+	x[3] = 82
+	x[4] = 83
+	x[5] = 93
 
+	//with a range
+	var t float64 = 0
+	for _, v := range x {
+		t += v;
+	}
+	fmt.Println("Range total: ", t / float64(len(x)))
 	var total float64 = 0
-	for i :=0; i < 5; i++ {
+	for i :=0; i < len(x); i++ {
 		total += x[i]
 	}
 
-	fmt.Println(total /5)
+	fmt.Println(total / float64(len(x)))
 }
