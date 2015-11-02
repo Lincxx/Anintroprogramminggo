0d746a9905d7623be1de1f39b0457954147633fa
bossco <bossco28@gmail.com>
2015-11-02 13:38:25 -0500

    Added in Ch8 about Pointer, and Ch9 about struct

---
+++ ch9/strutCircle/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+import "math"
+
+type Circle struct  {
+	x float64
+	y float64
+	r float64
+}
+
+func circleArea(c Circle) float64{
+	return math.Pi * c.r * c.r
+}
+
+func main() {
+	//var c Circle
+
+	c := Circle{x: 0, y: 0, r: 5}
+	//or like this if we know the order
+	//c := Circle{0,0,5}
+
+	fmt.Println(c.x, c.y, c.r)
+	fmt.Println(circleArea(c))
+	c.x = 10
+	c.y = 5
+
+
+
+
+}
